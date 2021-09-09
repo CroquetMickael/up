a37b29ee2854ac034178c55ceb3edfd7d3afc81d
unknown <mickael.croquet@laposte.net>
2021-09-09 11:21:51 +0200

    refactor(App): css and remove fetch call

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { useFetch } from "./hooks/useFetch";
-import StormDb from "stormdb";
 
 function App() {
-  const { get, data, hasError, httpCode, isResolved } = useFetch();
-
-  useEffect(() => {
-    get("/api/");
-  }, [get]);
-
-  useEffect(() => {
-    const engine = new StormDb.browserEngine("up");
-    const db = new StormDb(engine);
-    if (isResolved) {
-      if (!db.get("user").value()) {
-        db.set("user", {
-          name: data.name,
-          id: data.steam_id,
-        }).save();
-        console.log("saved");
-      }
-      console.log(data);
-      console.log(httpCode);
-    }
-    if (hasError) {
-      console.log("error", httpCode);
-    }
-  }, [data, hasError, httpCode, isResolved]);
-
   return (
     <div className="App">
       <header className="App-header">
