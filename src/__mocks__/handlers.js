5d658e59e4eec50928591cbfa1dac9da253dff97
unknown <mickael.croquet@laposte.net>
2021-09-11 02:49:35 +0200

    feat(Mocks): adding 3 new endpoints

--- src/__mocks__/handlers.js
+++ src/__mocks__/handlers.js
@@ -1,6 +1,9 @@
 import { rest } from "msw";
 import ping from "./fixtures/ping.json";
 import upload from "./fixtures/upload.json";
+import replays from "./fixtures/replays.json";
+import replaysData from "./fixtures/replaysData.json";
+import DataPerRank from "./fixtures/DataPerRank.json";
 
 import { apiURL } from "../hooks/useFetch";
 
@@ -11,4 +14,17 @@ export const handlers = [
   rest.post(`${apiURL}/v2/upload`, (req, res, ctx) => {
     return res(ctx.delay(), ctx.status(201), ctx.json(upload));
   }),
+  rest.get(`${apiURL}/replays`, (req, res, ctx) => {
+    return res(ctx.delay(), ctx.status(200), ctx.json(replays));
+  }),
+  rest.get(`${apiURL}/replays/:id`, (req, res, ctx) => {
+    const { id } = req.params;
+    return res(ctx.delay(), ctx.status(200), ctx.json(replaysData[id]));
+  }),
+  rest.get(
+    `https://ballchasing.com/population/average/stats`,
+    (req, res, ctx) => {
+      return res(ctx.delay(), ctx.status(200), ctx.json(DataPerRank));
+    }
+  ),
 ];
