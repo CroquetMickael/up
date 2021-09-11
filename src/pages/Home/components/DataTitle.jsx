b49c9b3f69dea09512e86d4d1b118eb13e8b8d20
unknown <mickael.croquet@laposte.net>
2021-09-11 18:50:48 +0200

    feat(Home): Using compared games in sections

--- src/pages/Home/components/DataTitle.jsx
+++ src/pages/Home/components/DataTitle.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const DataTitle = ({ Title }) => (
   <div className="flex flex-wrap w-full mt-8 px-6">
     <div className="w-full mb-6 lg:mb-0">
-      <h1 className="sm:text-4xl text-5xl font-medium title-font mb-2">
+      <h1 className="sm:text-4xl text-5xl font-medium title-font mb-2 text-white">
         {Title}
       </h1>
-      <div className="h-1 w-20 bg-indigo-500 rounded"></div>
+      <div className="h-1 w-20 bg-brandSub rounded"></div>
     </div>
   </div>
 );
