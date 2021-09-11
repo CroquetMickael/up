e22708afd0fae5150819ed34c0eb3138f330d24b
unknown <mickael.croquet@laposte.net>
2021-09-11 02:50:11 +0200

    fix(Upload): fixing width issue in upload modal

--- src/components/UploadReplay/UploadModal.jsx
+++ src/components/UploadReplay/UploadModal.jsx
@@ -19,11 +19,11 @@ const UploadModal = ({ modalOpen, toggleModal }) => {
             d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
           />
         </svg>
-        <div className="input_field flex flex-col w-max mx-auto text-center">
+        <div className="input_field flex flex-col mx-auto text-center">
           <div className="title text-indigo-500 uppercase">
             Upload to ballchasing
           </div>
-          You will be able to add the file change the visibility of the replay
+          <p>You will be able to change the visibility of uploaded replay</p>
         </div>
       </div>
     </Modal>
