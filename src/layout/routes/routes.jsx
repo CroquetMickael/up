c455738adb26ddb86b4b507741811f967e071400
unknown <mickael.croquet@laposte.net>
2021-09-12 02:11:41 +0200

    feat(Replays): Using replay context in Home page

--- src/layout/routes/routes.jsx
+++ src/layout/routes/routes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { UploadReplay } from "../../components/UploadReplay/UploadReplay";
+import { ReplayContextProvider } from "../../context/Replays/ReplaysContext";
 import { UserContextProvider } from "../../context/UserContext";
 import { Activate } from "../../pages/Activate/Activate";
 import { Home } from "../../pages/Home/Home";
@@ -10,16 +11,18 @@ const Routes = () => (
   <Layout>
     <BrowserRouter>
       <UserContextProvider>
-        <Switch>
-          <Route exact path="/activate">
-            <Activate />
-          </Route>
-          <UploadReplay>
-            <Route exact path="/">
-              <Home />
+        <ReplayContextProvider>
+          <Switch>
+            <Route exact path="/activate">
+              <Activate />
             </Route>
-          </UploadReplay>
-        </Switch>
+            <UploadReplay>
+              <Route exact path="/">
+                <Home />
+              </Route>
+            </UploadReplay>
+          </Switch>
+        </ReplayContextProvider>
       </UserContextProvider>
     </BrowserRouter>
   </Layout>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,79 +1,19 @@
-import React, { useMemo, useRef } from "react";
-import "./Home.css";
-import { useEffect, useState } from "react/cjs/react.development";
+import React from "react";
+import { useEffect } from "react/cjs/react.development";
 import { useUser } from "../../context/UserContext";
 import { useFetch } from "../../hooks/useFetch";
-import { useDB } from "../../hooks/useDB";
-import Bottleneck from "bottleneck";
-import { getReplaysData, UpdateReplaysData } from "./Home.service";
 import { useReplayData } from "../../hooks/useReplayData";
 import { BoostSection } from "./Section/BoostSection";
 import { MovementSection } from "./Section/MovementSection";
+import { useReplays } from "../../context/Replays/ReplaysContext";
+
+import "./Home.css";
 
 function Home() {
-  const { user, isFirstLoading, setIsFirstLoading } = useUser();
-  const { get, data, isResolved } = useFetch();
-  const {
-    get: getReplayData,
-    data: replayData,
-    isResolved: replayResolved,
-  } = useFetch();
+  const { user } = useUser();
   const { get: getRanksData, data: rankData } = useFetch();
-  const { DBSet, DBSave, DBGet } = useDB();
-  const [lastGames, setLastGames] = useState(
-    DBGet("lastReplays").value() || []
-  );
-  const [comparedGames, setComparedGames] = useState(
-    DBGet("comparedReplays").value() || []
-  );
-  const { boost, movement } = useReplayData({
-    lastGames,
-    comparedGames,
-    user,
-  });
-
-  const limiter = useMemo(
-    () =>
-      new Bottleneck({
-        minTime: 500,
-        maxConcurrent: 1,
-        reservoir: 20,
-        reservoirRefreshInterval: 1000,
-        reservoirRefreshAmount: 2,
-        trackDoneStatus: true,
-      }),
-    []
-  );
-
-  useEffect(() => {
-    get(`/replays?player-id=steam:${user?.id}`);
-  }, [get, user?.id]);
-
-  useEffect(() => {
-    if (isResolved && isFirstLoading) {
-      DBSet("lastReplays", []);
-      DBSet("comparedReplays", []);
-      DBSave();
-      getReplaysData({
-        data,
-        getReplayData,
-        DBSave,
-        DBSet,
-        limiter,
-        setIsFirstLoading,
-      });
-    }
-  }, [
-    DBSave,
-    DBSet,
-    data,
-    getReplayData,
-    isFirstLoading,
-    isResolved,
-    limiter,
-    setIsFirstLoading,
-  ]);
-
+  const { lastGames, comparedGames } = useReplays();
+  const { boost, movement } = useReplayData({ lastGames, comparedGames, user });
   useEffect(() => {
     getRanksData(
       "/population/average/stats?group=grank&min-rank=0&max-rank=0",
@@ -81,31 +21,6 @@ function Home() {
       true
     );
   }, [getRanksData]);
-
-  useEffect(() => {
-    const replaysData = DBGet("lastReplays").value() || [];
-    const comparedReplays = DBGet("comparedReplays").value() || [];
-    const gamesId = DBGet("gamesId").value();
-    const doneLimiter = limiter.counts().DONE === 20;
-
-    if (replayResolved) {
-      UpdateReplaysData({
-        replayData,
-        replaysData,
-        DBSet,
-        DBSave,
-        comparedReplays,
-        gamesId,
-      });
-    }
-
-    const maxReplayFound = replaysData.length === 10;
-    const maxComparedReplays = comparedReplays.length === 10;
-    if (doneLimiter && maxReplayFound && maxComparedReplays) {
-      setLastGames(replaysData);
-      setComparedGames(comparedReplays);
-    }
-  }, [DBGet, DBSave, DBSet, limiter, replayData, replayResolved]);
 
   return (
     <>
