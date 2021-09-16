91acba9ec9f9e48b90994c31e7af360395d4231c
unknown <mickael.croquet@laposte.net>
2021-09-16 18:17:12 +0200

    feat(Disclaimer): Alpha disclaimer

--- src/layout/routes/routes.jsx
+++ src/layout/routes/routes.jsx
@@ -17,6 +17,17 @@ const Routes = () => (
           </Route>
           <ReplayContextProvider>
             <UploadReplay>
+              <div class="bg-yellow-100 border-l-4 border-yellow-500 text-orange-700 p-4" role="alert">
+                <p class="font-bold">Be Warned</p>
+                <p>Lot of things to modify / fixed, it's still an alpha !</p>
+                <p>I know these ones : </p>
+                <ul>
+                  <li>- If ballChasing crash, you will never find the problem right now, press : <br /> CTRL + SHIFT + I {"=>"} network panel and if a call is in red, that's why it's not working</li>
+                  <li>- Some UI/UX stuff can be broken as the <strong>Tracking replays</strong></li>
+                  <li>- To upload, just drop a replay file in the application, currently no verification about the file type, so if the upload fail, you won't be informed</li>
+                  <li>- I will try my best to have more langage, currently only english :/</li>
+                </ul>
+              </div>
               <Route exact path="/">
                 <Home />
               </Route>
