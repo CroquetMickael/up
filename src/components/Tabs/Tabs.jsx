76933c4412b06c641b724788a86cc85b11cc3263
unknown <mickael.croquet@laposte.net>
2021-09-17 21:41:14 +0200

    refactor(Tabs): works with production + easy usage

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,42 +1,27 @@
-import React, { useState } from "react";
+import React from "react"
+import { useState } from "react";
 
 import "./Tabs.css"
 
-const TabItem = (props) => <div {...props} />;
-
-const Tabs = ({ defaultIndex = 0, onTabClick, children }) => {
-    const [bindIndex, setBindIndex] = useState(defaultIndex);
-    const changeTab = newIndex => {
-        if (typeof onTabClick === 'function') onTabClick(newIndex);
-        setBindIndex(newIndex);
-    };
-    const items = children.filter(item => item.type.name === 'TabItem');
-
+const Tabs = ({ tabNames, componentList }) => {
+    const [activeTab, setActiveTab] = useState(0);
     return (
         <>
             <div className="tab-menu sticky top-0 bg-brand z-10">
-                {items.map(({ props: { index, label } }) => (
+                {tabNames.map((name, index) =>
                     <button
-                        key={`tab-btn-${index}`}
-                        onClick={() => changeTab(index)}
-                        className={bindIndex === index ? 'focus' : ''}
+                        onClick={() => setActiveTab(index)}
+                        className={activeTab === index ? 'focus' : ''}
                     >
-                        {label}
-                    </button>
-                ))}
+                        {name}
+                    </button>)}
             </div>
             <div className="tab-view">
-                {items.map(({ props }) => (
-                    <div
-                        {...props}
-                        className={`tab-content ${bindIndex === props.index ? 'selected' : ''
-                            }`}
-                        key={`tab-content-${props.index}`}
-                    />
-                ))}
+                {componentList.map((component, index) => <>
+                    {activeTab === index ? component : null}
+                </>)}
             </div>
-        </>
-    );
-};
+        </>)
+}
 
-export { Tabs, TabItem }
+export { Tabs }
