9e0c157d7ff29d913b6519cc30315e7d499570e7
unknown <mickael.croquet@laposte.net>
2021-09-22 22:32:43 +0200

    feat(Settings): Adding settings page

--- src/layout/routes/routes.jsx
+++ src/layout/routes/routes.jsx
@@ -8,6 +8,7 @@ import { Activate } from "../../pages/Activate/Activate";
 import { Home } from "../../pages/Home/Home";
 import { ReplayDetail } from "../../pages/Replays/Detail/ReplayDetail";
 import { Replays } from "../../pages/Replays/Replays";
+import { Settings } from "../../pages/Settings/Settings";
 import { Layout } from "../Layout";
 
 const Routes = () => (
@@ -19,26 +20,42 @@ const Routes = () => (
           <ReplayContextProvider>
             <Layout>
               <UploadReplay>
-                <div className="bg-yellow-100 border-l-4 border-yellow-500 text-orange-700 p-4" role="alert">
+                <div
+                  className="bg-yellow-100 border-l-4 border-yellow-500 text-orange-700 p-4"
+                  role="alert"
+                >
                   <p className="font-bold">Be Warned</p>
                   <p>Lot of things to modify / fixed, it's still an alpha !</p>
                   <p>I know these ones: </p>
                   <ul>
-                    <li>- Some UI/UX stuff can be broken as the <strong>Tracking replays</strong></li>
-                    <li>- To upload, just drop a replay file in the application</li>
-                    <li>- I will try my best to have more langage, currently only english: /</li>
+                    <li>
+                      - Some UI/UX stuff can be broken as the{" "}
+                      <strong>Tracking replays</strong>
+                    </li>
+                    <li>
+                      - To upload, just drop a replay file in the application
+                    </li>
+                    <li>
+                      - I will try my best to have more langage, currently only
+                      english: /
+                    </li>
                   </ul>
                 </div>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/replays" component={Replays} />
-                <Route exact path="/replays/:replayId" component={ReplayDetail} />
+                <Route
+                  exact
+                  path="/replays/:replayId"
+                  component={ReplayDetail}
+                />
+                <Route exact path="/settings" component={Settings} />
               </UploadReplay>
             </Layout>
           </ReplayContextProvider>
         </Switch>
       </UserContextProvider>
     </NotificationContextProvider>
-  </HashRouter >
+  </HashRouter>
 );
 
 export { Routes };
